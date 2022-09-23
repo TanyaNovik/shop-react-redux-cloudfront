91c3f784b4f120a3cb24aa778d7c16a5e27de751
Tatsiana Novik <Tatsiana_Novik@epam.com>
2022-09-23 09:56:11 +0300

    feat: add cloudformation

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            My vegetable Store!
           </Link>
         </Typography>
 
